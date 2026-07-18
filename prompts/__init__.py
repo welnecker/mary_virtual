95a52e49d81db8a5f77074ab8f7929646b6c7e8b
welnecker <janiofdr@gmail.com>
2026-07-18 11:07:54 -0300

    Update __init__.py

--- prompts/__init__.py
+++ prompts/__init__.py
@@ -10,11 +10,24 @@ from prompts.emotional import (
     normalizar_estagio_emocional,
     obter_prompt_emocional,
 )
+from prompts.sexual import (
+    SEXUAL_LEVEL_ATTRACTION,
+    SEXUAL_LEVEL_DEEP_INTIMACY,
+    SEXUAL_LEVEL_DESIRE,
+    SEXUAL_LEVEL_FLIRT,
+    SEXUAL_LEVEL_INTIMACY,
+    SEXUAL_LEVEL_NONE,
+    montar_contexto_estado_sexual,
+    normalizar_fase_sexual,
+    normalizar_nivel_sexual,
+    obter_prompt_sexual,
+)
 from prompts.voice import (
     contar_cliches_ia,
     encontrar_cliches_ia,
     obter_prompt_voz,
 )
+
 
 __all__ = [
     "obter_prompt_base",
@@ -30,4 +43,14 @@ __all__ = [
     "normalizar_estagio_emocional",
     "limitar_transicao_emocional",
     "obter_prompt_emocional",
+    "SEXUAL_LEVEL_NONE",
+    "SEXUAL_LEVEL_ATTRACTION",
+    "SEXUAL_LEVEL_FLIRT",
+    "SEXUAL_LEVEL_DESIRE",
+    "SEXUAL_LEVEL_INTIMACY",
+    "SEXUAL_LEVEL_DEEP_INTIMACY",
+    "normalizar_nivel_sexual",
+    "normalizar_fase_sexual",
+    "montar_contexto_estado_sexual",
+    "obter_prompt_sexual",
 ]
