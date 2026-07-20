56180613524edc203c3d9bc211d5cf4c2f2bf23c
welnecker <janiofdr@gmail.com>
2026-07-20 10:17:34 -0300

    Create __init__.py

---
+++ scenarios/__init__.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+from scenarios.registry import (
+    iniciar_instancia_cenario,
+    listar_cenarios_disponiveis,
+    obter_cenario,
+)
+
+__all__ = [
+    "iniciar_instancia_cenario",
+    "listar_cenarios_disponiveis",
+    "obter_cenario",
+]
