bf11e64428c0cb20f397e75344c9cbaa5f81a2a6
welnecker <janiofdr@gmail.com>
2026-07-21 12:32:50 -0300

    Create repositories package

---
+++ repositories/__init__.py
@@ -0,0 +1,11 @@
+from repositories.user_repository import (
+    criar_usuario_com_credenciais,
+    obter_usuario_por_email_normalizado,
+    registrar_login_usuario,
+)
+
+__all__ = [
+    "criar_usuario_com_credenciais",
+    "obter_usuario_por_email_normalizado",
+    "registrar_login_usuario",
+]
