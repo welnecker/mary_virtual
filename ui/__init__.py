d9d8fce947cd91745d131337e15be0c6a7dca889
welnecker <janiofdr@gmail.com>
2026-07-21 12:36:53 -0300

    Export authentication interface

--- ui/__init__.py
+++ ui/__init__.py
@@ -1,1 +1,14 @@
+from ui.login import (
+    AUTH_ACTION_LOGIN,
+    AUTH_ACTION_REGISTER,
+    AuthenticationResult,
+    renderizar_tela_login,
+)
 
+
+__all__ = [
+    "AUTH_ACTION_LOGIN",
+    "AUTH_ACTION_REGISTER",
+    "AuthenticationResult",
+    "renderizar_tela_login",
+]
