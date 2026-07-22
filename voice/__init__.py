f3c3c69480d0f99ec6ee658752ae063779303198
welnecker <janiofdr@gmail.com>
2026-07-22 20:48:43 -0300

    feat: expose ElevenLabs voice provider

---
+++ voice/__init__.py
@@ -0,0 +1,20 @@
+from voice.elevenlabs_tts import (
+    ELEVENLABS_MODEL_ID,
+    ELEVENLABS_TTS_VERSION,
+    ElevenLabsTTSError,
+    VOICE_SETTINGS,
+    elevenlabs_configurada,
+    obter_credenciais,
+    sintetizar_fala,
+)
+
+
+__all__ = [
+    "ELEVENLABS_MODEL_ID",
+    "ELEVENLABS_TTS_VERSION",
+    "ElevenLabsTTSError",
+    "VOICE_SETTINGS",
+    "elevenlabs_configurada",
+    "obter_credenciais",
+    "sintetizar_fala",
+]
