a81a05d716aab8e2544cf7dd19a5ca2475129a0b
welnecker <janiofdr@gmail.com>
2026-07-22 16:20:14 -0300

    Install safe interaction rerun optimizer

--- ui/__init__.py
+++ ui/__init__.py
@@ -4,11 +4,17 @@ from ui.app_runtime_integration import (
     aplicar_politica_adaptativa_encerramento,
     install_app_runtime_integration,
 )
+from ui.interaction_rerun_optimizer import (
+    INTERACTION_RERUN_OPTIMIZER_VERSION,
+    aplicar_otimizacao_rerun,
+    install_interaction_rerun_optimizer,
+)
 
 
-# O pacote ui é carregado antes de main(). O instalador apenas envolve st.title;
+# O pacote ui é carregado antes de main(). Os instaladores envolvem st.title;
 # a integração real acontece depois que app.py já definiu todas as funções.
 install_app_runtime_integration()
+install_interaction_rerun_optimizer()
 
 
 from ui.login import (
@@ -21,9 +27,12 @@ from ui.login import (
 
 __all__ = [
     "APP_RUNTIME_INTEGRATION_VERSION",
+    "INTERACTION_RERUN_OPTIMIZER_VERSION",
     "aplicar_integracao_runtime",
+    "aplicar_otimizacao_rerun",
     "aplicar_politica_adaptativa_encerramento",
     "install_app_runtime_integration",
+    "install_interaction_rerun_optimizer",
     "AUTH_ACTION_LOGIN",
     "AUTH_ACTION_REGISTER",
     "AuthenticationResult",
