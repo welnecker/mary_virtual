63b4993096ae7beac24bfa94a9e60be046943d61
welnecker <janiofdr@gmail.com>
2026-07-27 11:20:34 -0300

    Export public profile identity operations

---
+++ identity/__init__.py
@@ -0,0 +1,9 @@
+from .public_profile import (
+    aplicar_atualizacoes_perfil_publico,
+    montar_perfil_publico,
+)
+
+__all__ = [
+    "montar_perfil_publico",
+    "aplicar_atualizacoes_perfil_publico",
+]
