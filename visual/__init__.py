9dadee0936e3424336cef1683ffe8f13bb061709
welnecker <janiofdr@gmail.com>
2026-07-27 10:24:19 -0300

    Add visual state domain package

---
+++ visual/__init__.py
@@ -0,0 +1,9 @@
+from .state import (
+    DEFAULT_VISUAL_MEMORY_STATE,
+    criar_estado_memoria_visual_padrao,
+)
+
+__all__ = [
+    "DEFAULT_VISUAL_MEMORY_STATE",
+    "criar_estado_memoria_visual_padrao",
+]
