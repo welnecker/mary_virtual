0fd5319b6246cea3b3713afd4804336e1a65c202
welnecker <janiofdr@gmail.com>
2026-07-27 10:30:36 -0300

    Export variable appearance state

--- visual/__init__.py
+++ visual/__init__.py
@@ -1,9 +1,15 @@
+from .appearance_state import (
+    DEFAULT_VARIABLE_APPEARANCE_STATE,
+    criar_estado_aparencia_variavel_padrao,
+)
 from .state import (
     DEFAULT_VISUAL_MEMORY_STATE,
     criar_estado_memoria_visual_padrao,
 )
 
 __all__ = [
+    "DEFAULT_VARIABLE_APPEARANCE_STATE",
+    "criar_estado_aparencia_variavel_padrao",
     "DEFAULT_VISUAL_MEMORY_STATE",
     "criar_estado_memoria_visual_padrao",
 ]
