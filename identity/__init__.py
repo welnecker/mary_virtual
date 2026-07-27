2048be2ae99db7f8f9600a15dcc588eba5d9c394
welnecker <janiofdr@gmail.com>
2026-07-27 12:14:49 -0300

    Export identity state factories

--- identity/__init__.py
+++ identity/__init__.py
@@ -2,8 +2,20 @@ from .public_profile import (
     aplicar_atualizacoes_perfil_publico,
     montar_perfil_publico,
 )
+from .state import (
+    DEFAULT_IDENTITY_STATE,
+    DEFAULT_PUBLIC_PROFILE_IMAGE_PATH,
+    DEFAULT_PUBLIC_PROFILE_STATE,
+    criar_estado_identidade_padrao,
+    criar_estado_perfil_publico_padrao,
+)
 
 __all__ = [
     "montar_perfil_publico",
     "aplicar_atualizacoes_perfil_publico",
+    "DEFAULT_PUBLIC_PROFILE_IMAGE_PATH",
+    "DEFAULT_IDENTITY_STATE",
+    "DEFAULT_PUBLIC_PROFILE_STATE",
+    "criar_estado_identidade_padrao",
+    "criar_estado_perfil_publico_padrao",
 ]
