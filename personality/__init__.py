0414b754569511d6f073489dd6e8406d959b9c5b
welnecker <janiofdr@gmail.com>
2026-07-27 12:20:39 -0300

    Add personality domain exports

---
+++ personality/__init__.py
@@ -0,0 +1,9 @@
+from .state import (
+    DEFAULT_PERSONALITY_STATE,
+    criar_estado_personalidade_padrao,
+)
+
+__all__ = [
+    "DEFAULT_PERSONALITY_STATE",
+    "criar_estado_personalidade_padrao",
+]
