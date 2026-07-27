a5c7550b0adbb2e5f3d48344e06c8074ac19a701
welnecker <janiofdr@gmail.com>
2026-07-27 12:24:58 -0300

    refactor: export virtual context state

---
+++ virtual_context/__init__.py
@@ -0,0 +1,9 @@
+from .state import (
+    DEFAULT_VIRTUAL_CONTEXT_STATE,
+    criar_estado_contexto_virtual_padrao,
+)
+
+__all__ = [
+    "DEFAULT_VIRTUAL_CONTEXT_STATE",
+    "criar_estado_contexto_virtual_padrao",
+]
