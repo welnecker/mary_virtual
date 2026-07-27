271f74f89383b6f5a9b0130bf5bdb807f74474a5
welnecker <janiofdr@gmail.com>
2026-07-27 10:35:47 -0300

    Export visual appearance operations

--- visual/__init__.py
+++ visual/__init__.py
@@ -1,3 +1,9 @@
+from .appearance import (
+    VARIABLE_APPEARANCE_KEYS,
+    aplicar_aparencia_variavel,
+    extrair_aparencia_variavel,
+    extrair_tracos_fisicos_estaveis,
+)
 from .appearance_state import (
     DEFAULT_VARIABLE_APPEARANCE_STATE,
     criar_estado_aparencia_variavel_padrao,
@@ -8,6 +14,10 @@ from .state import (
 )
 
 __all__ = [
+    "VARIABLE_APPEARANCE_KEYS",
+    "aplicar_aparencia_variavel",
+    "extrair_aparencia_variavel",
+    "extrair_tracos_fisicos_estaveis",
     "DEFAULT_VARIABLE_APPEARANCE_STATE",
     "criar_estado_aparencia_variavel_padrao",
     "DEFAULT_VISUAL_MEMORY_STATE",
