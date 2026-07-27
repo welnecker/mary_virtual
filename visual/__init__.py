f6e29853ee61feccf6de792be4f19b1df3040946
welnecker <janiofdr@gmail.com>
2026-07-27 12:18:12 -0300

    Export physical profile state

--- visual/__init__.py
+++ visual/__init__.py
@@ -9,6 +9,10 @@ from .appearance_state import (
     criar_estado_aparencia_variavel_padrao,
 )
 from .memory import registrar_imagem_aprovada_no_perfil
+from .physical_state import (
+    DEFAULT_PHYSICAL_PROFILE_STATE,
+    criar_estado_perfil_fisico_padrao,
+)
 from .state import (
     DEFAULT_VISUAL_MEMORY_STATE,
     criar_estado_memoria_visual_padrao,
@@ -22,6 +26,8 @@ __all__ = [
     "DEFAULT_VARIABLE_APPEARANCE_STATE",
     "criar_estado_aparencia_variavel_padrao",
     "registrar_imagem_aprovada_no_perfil",
+    "DEFAULT_PHYSICAL_PROFILE_STATE",
+    "criar_estado_perfil_fisico_padrao",
     "DEFAULT_VISUAL_MEMORY_STATE",
     "criar_estado_memoria_visual_padrao",
 ]
