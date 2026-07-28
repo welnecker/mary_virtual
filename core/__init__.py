3d583805fc5db8a00a7c7dc2d86bb6018b665014
welnecker <janiofdr@gmail.com>
2026-07-28 12:43:25 -0300

    Create generic story engine package

---
+++ core/__init__.py
@@ -0,0 +1,21 @@
+from .story_engine import StoryEngine
+from .story_models import (
+    Beat,
+    Chapter,
+    GateDecision,
+    MaryProfile,
+    StoryManifest,
+    StorySession,
+    TurnPlan,
+)
+
+__all__ = [
+    "Beat",
+    "Chapter",
+    "GateDecision",
+    "MaryProfile",
+    "StoryEngine",
+    "StoryManifest",
+    "StorySession",
+    "TurnPlan",
+]
