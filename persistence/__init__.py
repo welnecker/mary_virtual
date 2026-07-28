b1649cff8cc8063f481734d1d607e2c088794ca0
welnecker <janiofdr@gmail.com>
2026-07-28 14:01:27 -0300

    Add persistence package for story runtime

---
+++ persistence/__init__.py
@@ -0,0 +1,19 @@
+from .story_sheets import (
+    create_runtime_session,
+    create_story_session,
+    finish_runtime_session,
+    load_catalog_overrides,
+    load_story_messages,
+    persist_interaction,
+    persist_story_session,
+)
+
+__all__ = [
+    "create_runtime_session",
+    "create_story_session",
+    "finish_runtime_session",
+    "load_catalog_overrides",
+    "load_story_messages",
+    "persist_interaction",
+    "persist_story_session",
+]
