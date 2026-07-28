353c3f8fc41c54828e69e8e377b4dee1776d1076
welnecker <janiofdr@gmail.com>
2026-07-28 14:39:49 -0300

    Export story resume helpers

--- persistence/__init__.py
+++ persistence/__init__.py
@@ -1,3 +1,8 @@
+from .story_resume import (
+    catalog_story_state,
+    hydrate_story_session,
+    latest_story_sessions_by_scenario,
+)
 from .story_sheets import (
     create_runtime_session,
     create_story_session,
@@ -9,9 +14,12 @@ from .story_sheets import (
 )
 
 __all__ = [
+    "catalog_story_state",
     "create_runtime_session",
     "create_story_session",
     "finish_runtime_session",
+    "hydrate_story_session",
+    "latest_story_sessions_by_scenario",
     "load_catalog_overrides",
     "load_story_messages",
     "persist_interaction",
