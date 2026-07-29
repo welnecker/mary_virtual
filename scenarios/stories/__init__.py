d915dfff8f15d6f98c9af54c02d78a71b8b3a2da
welnecker <janiofdr@gmail.com>
2026-07-29 09:44:54 -0300

    Register definitive story packages

--- scenarios/stories/__init__.py
+++ scenarios/stories/__init__.py
@@ -1,1 +1,13 @@
+from __future__ import annotations
 
+from scenarios.engine.registry import story_registry
+from scenarios.stories.casada_frustrada.story import STORY as CASADA_FRUSTRADA
+
+
+def register_stories() -> None:
+    registered = {story.story_id for story in story_registry.list_stories()}
+    if CASADA_FRUSTRADA.story_id not in registered:
+        story_registry.register(CASADA_FRUSTRADA)
+
+
+__all__ = ["CASADA_FRUSTRADA", "register_stories"]
