ab4e0710a5dc8418839ec5b6f9f806e869bd172f
welnecker <janiofdr@gmail.com>
2026-07-29 08:58:00 -0300

    Add Casada Frustrada v2 card

---
+++ scenarios/stories_v2/casada_frustrada/card.py
@@ -0,0 +1,10 @@
+from __future__ import annotations
+
+CARD = {
+    "story_id": "casada_frustrada",
+    "initial_chapter_id": "cap_01",
+    "title": "Casada frustrada",
+    "description": "Uma história interativa em capítulos.",
+    "character_id": "mary",
+    "engine": "v2",
+}
